bc048e3b8aa67251824965b23fe518ae49e70937
kurochkaand <kurochka.and@gmail.com>
2025-07-21 13:30:34 +0200

    Removed duplicated function

--- data_processor.py
+++ data_processor.py
@@ -1,18 +1,5 @@
 # data_processor.py
 import pandas as pd
-from scipy.interpolate import interp1d
-
-def merge_a_b(a_df, b_df):
-    """Interpolates and combines two spectra."""
-    common_x = sorted(set(a_df["Wavenumber"]).union(b_df["Wavenumber"]))
-    merged = pd.DataFrame({"Wavenumber": common_x})
-    for col in a_df.columns[1:]:
-        f_a = interp1d(a_df["Wavenumber"], a_df[col], bounds_error=False, fill_value=0)
-        f_b = interp1d(b_df["Wavenumber"], b_df[col], bounds_error=False, fill_value=0)
-        merged[col] = (f_a(common_x) + f_b(common_x)) / 2
-    return merged
-# data_processor.py
-import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d
 
